3c97285e02b6617a9eff9b05a6b894bbe8b323e1
natan7366 <natan7366@gmail.com>
2020-10-15 17:11:20 +0300

    adding connectionString

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const myDB = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssl: true
     // user : 'postgres',
     // password : 'test',
